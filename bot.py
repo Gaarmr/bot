c102a89a2234e0a8b4b0087db32f05d2b439b664
Gaarmr <vdnv@bk.ru>
2021-10-04 05:03:08 +0600

    create dice

--- bot.py
+++ bot.py
@@ -1,34 +1,69 @@
+from rules_eng import rules
 import settings
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, commandhandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from random import randint
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
-def guess_number(update, context):
+gain=100
+
+def play_dice(user_number, bet, gain):
+    if user_number<2 or user_number>12:
+        message = 'Number out of range. \nPlease enter a number from 2 to 12' 
+    elif bet<1 or bet>gain:
+        message = 'Bet is not allowed'
+    else:
+        score1 = randint(1,6)
+        score2 = randint(1,6)
+        total_score = score1 + score2
+        if total_score==user_number:
+            gain += 4 * bet
+            message = f"На первом кубике выпало {score1}. На втором кубике выпало {score2}. \nВы выйграли : {bet}х4, ваш счет {gain}"
+        elif total_score < 7 and user_number<7:
+            gain += bet
+            message = f"На первом кубике выпало {score1}. На втором кубике выпало {score2}. \nВы выйграли : {bet}, ваш счет {gain}"
+        elif total_score > 7 and user_number>7:
+            gain += bet
+            message = f"На первом кубике выпало {score1}. На втором кубике выпало {score2}. \nВы выйграли : {bet}, ваш счет {gain}"
+        else:
+            gain -= bet
+            message = f"На первом кубике выпало {score1}. На втором кубике выпало {score2}. \nСтавка проиграна, ваш счет {gain}"
+    return message
+
+
+def dice_number(update, context):
     print(context.args)
     if context.args:
         try: 
             user_number = int(context.args[0])
-            message = f"Let's go. Your number is {user_number}"
+            bet = int(context.args[1])
+            message = play_dice(user_number, bet, gain)
         except (TypeError, ValueError):
             message = 'Enter an integer'
     else:
-        message = 'Enter an integer \nUse /guess #number#'
+        #update.message.reply_text(rules)
+        message = '\nEnter an integer \nUse /dice #number# #bet#'
     update.message.reply_text(message)
+
+def show_rules(update, context):
+    update.message.reply_text(rules)
 
 def talk_to_me(update, context):
     user_text = update.message.text 
     print(user_text)
     update.message.reply_text(user_text)
+    update.message.reply_text('Hello! Commands: /start /rules /dice')
 
 def greet_user(update, context):
     print('Вызван /start')
-    update.message.reply_text('Hello! Commands: /start /guess')
+    update.message.reply_text('Hello! Commands: /start /rules /dice')
 
 def main():
     mybot = Updater(settings.API_KEY, use_context=True)
     dp = mybot.dispatcher
-    dp.add_handler(CommandHandler('guess', guess_number))
+    dp.add_handler(CommandHandler('rules', show_rules))
+    dp.add_handler(CommandHandler('dice', dice_number))
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
---
+++ rules_eng.py
@@ -0,0 +1,8 @@
+rules = ['At the start, the player has 100 points.',
+'The player names any number in the range from 2 to 12 and the bet that he makes this turn.',
+'The program selects two random numbers from 1 to 6 ("rolls a die" with numbers from 1 to 6 on the edges).',
+'If the sum of the drawn digits is less than 7 and the player conceives a number less than 7, he wins the placed bet.',
+'If the sum of the drawn numbers is more than 7 and the player conceives a number greater than 7, he also wins the bet made.',
+'If the player guesses the sum of the numbers, he gets four times more points than the bet made.',
+'The bet is lost if none of the situations described occurs.',
+'If all points are used up, the game ends, after each attempt it is suggested to end the game.']
