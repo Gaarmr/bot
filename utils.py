874bcd197a715330fce68e541132b67087685452
Gaarmr <vdnv@bk.ru>
2021-10-08 12:09:03 +0600

    update clarifai

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
-from clarifai.rest import ClarifaiApp
+#from clarifai_grpc.grpc.api import service_pb2, resources_pb2, service_pb2_grpc
+#from clarifai_grpc.grpc.api.status import status_code_pb2
+#from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
+
 from emoji import emojize
-from pprint import PrettyPrinter
 from random import choice
 import settings
 from telegram import ReplyKeyboardMarkup, KeyboardButton
@@ -19,15 +21,24 @@ def get_smile(user_data):
         return emojize(smile, use_aliases=True)
     return user_data['emoji']
 
-def is_cat(file_name):
-    app = ClarifaiApp(api_key=settings.CLARIFAI_API_KEY)
-    model = app.public_models.general_model
-    response = model.predict_by_filename(file_name, max_concepts=5)
-    return response
+#def is_cat(user_photo):
+    stub = service_pb2_grpc.V2Stub(ClarifaiChannel.get_grpc_channel())
+    metadata = ('authorization', f'Key {settings.CLARIFAI_API_KEY}')
+    request = service_pb2.PostModelOutputsRequest(
+        model_id='aaa03c23b3724a16a56b629203edc62c',
+        inputs=[
+        resources_pb2.Input(data=resources_pb2.Data(image=resources_pb2.Image(url=user_photo)))
+        ])
+    response = stub.PostModelOutputs(request, metadata=metadata)   
+    if response.status.code != status_code_pb2.SUCCESS:
+        print("There was an error with your request!")
+        print("\tCode: {}".format(response.outputs[0].status.code))
+        print("\tDescription: {}".format(response.outputs[0].status.description))
+        print("\tDetails: {}".format(response.outputs[0].status.details))
+        raise Exception("Request failed, status code: " + str(response.status.code))
+    for concept in response.outputs[0].data.concepts:
+        print('%12s: %.2f' % (concept.name, concept.value))
 
-is_cat("img\pic_4.jpg")
-'''
-if __name__ == "__main__":
-        response = is_cat("img\pic_3.jpg")
-        print(response)
-'''
+#if __name__ == "__main__":
+    response = is_cat('img\pic_4.jpg')
+    print(response)
